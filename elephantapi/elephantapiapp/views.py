3be8d4596150c584dec6ffaacac51c7617057488
Rafael N <rafaelngoncalves5@outlook.com>
2023-03-29 18:37:28 -0300

    Project fully configured

--- elephantapi/elephantapiapp/views.py
+++ elephantapi/elephantapiapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('ola mundo!')
+    return JsonResponse({'status': 200, 'msg': 'OK' })
