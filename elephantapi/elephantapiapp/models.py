4f02933e3758a3d73acecc87526cfc07122c4114
Rafael N <rafaelngoncalves5@outlook.com>
2023-03-30 14:20:18 -0300

    Models

--- elephantapi/elephantapiapp/models.py
+++ elephantapi/elephantapiapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Weak entity
+class Amount(models.Model):
+    unities = models.IntegerField()
+
+class Country(models.Model):
+    id_country = models.AutoField(primary_key=True)
+    # Available sanctuaries in the country
+    amount_s = models.IntegerField()
+
+class Sanctuary(models.Model):
+    id_sanctuary = models.AutoField(primary_key=True)
+    fk_amount = models.ForeignKey(Amount, on_delete=models.CASCADE)
+    fk_country = models.ForeignKey(Country, on_delete=models.CASCADE)
